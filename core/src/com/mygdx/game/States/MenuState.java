2fdaa1526f0e7c9c679378a6f269d2bf371ffe86
IsaacShella <isaac.shella.14@gmail.com>
2015-12-09 10:41:45 -0600

    Added in a couple of background images Also changed what's being drawn as backgrounds

--- core/src/com/mygdx/game/States/MenuState.java
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -13,7 +13,7 @@ public class MenuState extends State{
     public MenuState(GameStateManager gsm) {
         super(gsm);
         //define game background and play button
-        background = new Texture("park2.png");
+        background = new Texture("menubackground.png");
         playBtn = new Texture("playbtn.png");
         //link to the tutorial activity
         tutorialBtn = new Texture("playbtn.png");
